053ca75c7e0f62990d6a6671905a7a310ff5d700
王培嘉 <jollyja@gmail.com>
2021-04-08 08:31:31 +0800

    test

--- src/main/java/cn/hello/c/controller/CController.java
+++ src/main/java/cn/hello/c/controller/CController.java
@@ -1,5 +1,9 @@
 package cn.hello.c.controller;
 
+import com.alibaba.nacos.api.annotation.NacosInjected;
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.naming.NamingService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +14,19 @@ import java.util.Map;
 @RestController
 public class CController {
 
+    @NacosValue(value = "${whoami:unkonwn}", autoRefreshed = true)
+    private String whoami;
+
+    @NacosInjected
+    private NamingService namingService;
+
     @PostMapping(path = "/c/confirm")
-    public Map<String, String> done(@RequestBody Map<String, String> req) {
+    public Map<String, String> confirm(@RequestBody Map<String, String> req) throws NacosException {
         Map<String, String> res = new HashMap<>();
         res.put("abc", "def");
+        res.put("whoami", whoami);
+        System.out.println("who am I : " + whoami);
+        System.out.println(namingService.getAllInstances("hello_c", "helloGroup"));
         return res;
     }
 
